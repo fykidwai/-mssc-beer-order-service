64839dfc4761f42fc561b62af99b567a17a190fe
Faiz Kidwai <f.y.kidwai@gmail.com>
2021-01-20 22:01:22 +0530

    add validation transitions. closes #11

--- src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
 import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
@@ -21,6 +22,20 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
             .end(BeerOrderStatusEnum.DELIVERED).end(BeerOrderStatusEnum.PICKED_UP)
             .end(BeerOrderStatusEnum.ALLOCATION_EXCEPTION).end(BeerOrderStatusEnum.DELIVERY_EXCEPTION)
             .end(BeerOrderStatusEnum.VALIDATION_EXCEPTION);
+    }
+
+    @Override
+    public void configure(final StateMachineTransitionConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> transitions)
+        throws Exception {
+        transitions.withExternal().source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.NEW)
+            .event(BeerOrderEventEnum.VALIDATE_ORDER)
+// TODO: Validation action to be added here
+            .and().withExternal().source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATED)
+            .event(BeerOrderEventEnum.VALIDATION_PASSED)
+
+            .and().withExternal().source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
+            .event(BeerOrderEventEnum.VALIDATION_FAILED);
+
     }
 
 }
