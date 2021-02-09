9271cda4ddc1fe1bf4f181bb970b8ce56ceb6a64
Faiz Kidwai <f.y.kidwai@gmail.com>
2021-02-09 20:36:31 +0530

    adding validation request action. closes #16

---
+++ src/main/java/guru/sfg/beer/order/service/sm/actions/ValidateOrderAction.java
@@ -0,0 +1,39 @@
+package guru.sfg.beer.order.service.sm.actions;
+
+import java.util.UUID;
+
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+
+import guru.sfg.beer.order.service.config.JmsConfig;
+import guru.sfg.beer.order.service.domain.BeerOrder;
+import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import guru.sfg.beer.order.service.repositories.BeerOrderRepository;
+import guru.sfg.beer.order.service.services.BeerOrderManagerImpl;
+import guru.sfg.beer.order.service.web.mappers.BeerOrderMapper;
+import guru.sfg.brewery.model.events.ValidateOrderRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class ValidateOrderAction implements Action<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+    private final BeerOrderRepository beerOrderRepository;
+    private final BeerOrderMapper beerOrderMapper;
+    private final JmsTemplate jmsTemplate;
+
+    @Override
+    public void execute(final StateContext<BeerOrderStatusEnum, BeerOrderEventEnum> context) {
+        final String beerOrderId = (String)context.getMessage().getHeaders().get(BeerOrderManagerImpl.ORDER_ID_HEADER);
+        final BeerOrder beerOrder = beerOrderRepository.findOneById(UUID.fromString(beerOrderId));
+        jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_QUEUE,
+            ValidateOrderRequest.builder().beerOrderDto(beerOrderMapper.beerOrderToDto(beerOrder)).build());
+        log.debug("Sent validation request to queue for " + beerOrderId);
+    }
+
+}
